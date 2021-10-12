e0405041d310eda35672cbe8c58c467f7047060b
WN20200003\nam_hyunjae <hzzn74@gmail.com>
2021-10-12 11:41:32 +0900

    edit js to ES6

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 window.addEventListener("load", function () {
-  if (isDesktopOS) {
+  if (isDesktop) {
     alert("you in PC");
   } else {
     alert("you in Mobile");
   }
 });
 
-function isDesktopOS() {
+const isDesktop = () => {
   return (
     "win16|win32|win64|windows|mac|macintel|linux|freebsd|openbsd|sunos".indexOf(
       navigator.platform.toLowerCase()
     ) >= 0
   );
-}
+};
