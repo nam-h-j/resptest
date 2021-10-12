3f4562fa928fc5663b0122ba558428b9794f710a
WN20200003\nam_hyunjae <hzzn74@gmail.com>
2021-10-12 12:33:32 +0900

    add some list for responsive layout

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+var md = new MobileDetect(navigator.userAgent);
+
+const makeList = (device) => {
+  let node = document.createElement("li");
+  let textnode = document.createTextNode(device);
+  let ul = document.getElementById("contents");
+  node.appendChild(textnode);
+
+  ul.appendChild(node);
+};
+
 window.addEventListener("load", function () {
   if (md.mobile() === null) {
-    alert("you in PC");
+    for (let i = 0; i < 8; i++) {
+      makeList("you in PC");
+    }
   } else {
-    alert("you in Mobile");
+    for (let i = 0; i < 10; i++) {
+      makeList("you in Mobile");
+    }
   }
 });
-
-var md = new MobileDetect(navigator.userAgent);
